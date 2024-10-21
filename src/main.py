ea3380a75fd9f47b0c97a15ef5e7b596a76edc83
Jan Zisenis <jan.zisenis@gmail.com>
2024-10-21 11:23:06 +0200

    added the sync functionality

--- src/main.py
+++ src/main.py
@@ -1,43 +1,106 @@
-import os
-import requests
-import sqlite3
-import time
-from datetime import datetime
-from dotenv import load_dotenv
+import logging
+from typing import List
+from smoobu import Smoobu, Booking
+from google_calendar import (
+    GoogleCalendarEvent,
+    EventTime,
+    EventAttendee,
+    EventReminder,
+    EventReminderOverride,
+    GoogleCalendar,
+)
+from database import Db
 
-load_dotenv()
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("app-main.log")
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
+def create_calendar_event(booking: Booking):
+    event = GoogleCalendarEvent(
+        summary=f"[{booking.reference_id}] {booking.guest_name}",
+        location=booking.apartment.name,
+        description=f"""
+        {booking.check_out}
+        Name: {booking.guest_name}
+        send message: {booking.guest_app_url}
+        """,
+        start=EventTime(booking.arrival),
+        end=EventTime(booking.departure),
+    )
+    return event
 
-def get_smoobu_data():
+def create_and_insert_google_calendar_events(bookings: List[Booking], db: Db):
     """
-    Get the data from smoobu api
-    :return: list of dict
+    Create and insert google calendar events to the database
+    :param bookings: list of bookings
+    :return: None
     """
-    url = f"https://api.smoobu.com/v1/customers?api_key={os.getenv('SMOOBU_API')}"
-    response = requests.get(url)
-    data = response.json()
-    return data
+    google_calendar = GoogleCalendar()
+    event = create_calendar_event(booking)
+    for booking in bookings:
+        event_id = google_calendar.create_google_calendar_event(event)
+        if event_id is None:
+            return
+        db.insert_google_calendar_event(booking, event_id)
+
+
+def delete_google_calendar_events(bookings: List[GoogleCalendarEvent], db: Db):
+    """
+    Delete the google calendar events from the database
+    :param bookings: list of bookings
+    :return: None
+    """
+    google_calendar = GoogleCalendar()
+    for booking in bookings:
+        # google_calendar.delete_google_calendar_event(booking.id)
+        db.delete_google_calendar_event(booking.id)
+
+def check_modified_bookings(bookings: List[Booking], db: Db):
+    """
+    Check if the bookings are modified in the database
+    :param bookings: list of bookings
+    :return: None
+    """
+    google_calendar = GoogleCalendar()
+    with Db() as db:
+        for booking in bookings:
+            calender_event_id = db.is_modified(booking.id, booking.modified_at)
+            if calender_event_id:
+                event = create_calendar_event(booking)
+                google_calendar.update_google_calendar_event(
+                    calender_event_id[0], event
+                )
+                db.update_modified_at(booking.id, booking.modified_at)
+                print(f"Booking {calender_event_id[0]} is modified")
+                logger.info(f"Booking {booking.id} is modified")
+
+
+
+def sync_smoobu_to_google_calendar():
+    logger.info("Starting the sync")
+    bookings = Smoobu().get_smoobu_reservations()
+    with Db() as db:
+        (
+            database_events_not_in_bookings,
+            bookings_not_in_database_events,
+        ) = db.get_entries_not_in_list(bookings)
+        logger.debug(f"Number of bookings not in the database: {len(bookings_not_in_database_events)}")
+        logger.debug(f"Number of database events not in bookings: {len(database_events_not_in_bookings)}")
+        if bookings_not_in_database_events:
+            create_and_insert_google_calendar_events(
+                bookings_not_in_database_events, db
+            )
+        if database_events_not_in_bookings:
+            delete_google_calendar_events(database_events_not_in_bookings, db)
+        check_modified_bookings(bookings, db)
 
 
 if __name__ == "__main__":
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    # Create a table for the google calendar events to the booking id from the smoobu api
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS google_calendar_events (
-            booking_id TEXT PRIMARY KEY,
-            event_id TEXT,
-            event_title TEXT,
-            event_start TEXT,
-            event_end TEXT
-        )
-        """
-    )
-    conn.commit()
-    # Get the data from smoobu api
-    smoobu_data = get_smoobu_data()
-    print(smoobu_data)
-
-
-
+    logger.setLevel(logging.DEBUG)
+    sync_smoobu_to_google_calendar()
